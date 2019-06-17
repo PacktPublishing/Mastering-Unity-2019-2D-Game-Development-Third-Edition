5b71992044c2547c6f3564d6079781ca7df7b6b2
Ashley Godbold <agodbold@adxnet.com>
2019-06-16 23:21:03 -0500

    forgot scripts in chapter files

---
+++ Chapter 04 Files/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour{
+    // Distance between protagonist and camera in horizontal direction 
+    public float xOffset = 0f;
+    // Distance between protagonist and camera in vertical direction 
+    public float yOffset = 0f;
+
+    // Reference to the protagonist's transform. 
+    public Transform protagonist;
+
+    void LateUpdate() {
+        this.transform.position = new Vector3(protagonist.transform.position.x +
+          xOffset, this.transform.position.y + yOffset, -10);
+    }
+}
---
+++ Chapter 04 Files/Sorting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sorting : MonoBehaviour{
+    Transform theObject;
+	SpriteRenderer theObjectSpriteRenderer;
+
+    Transform protagonist;
+    SpriteRenderer protagSpriteRenderer;
+
+    void Awake(){
+        theObject = GetComponent<Transform>();
+		theObjectSpriteRenderer = GetComponent<SpriteRenderer>();
+		protagonist = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        protagSpriteRenderer = protagonist.GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (theObject.position.y >= protagonist.position.y) {
+			theObjectSpriteRenderer.sortingOrder = (protagSpriteRenderer.sortingOrder) - 1;
+
+        }
+        if (theObject.position.y < protagonist.position.y) {
+			theObjectSpriteRenderer.sortingOrder = (protagSpriteRenderer.sortingOrder) + 1;
+        }
+    }
+}
